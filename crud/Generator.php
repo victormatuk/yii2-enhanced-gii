600de590b11134843a69ef8b3aa9b6c8bfb737c9
Matuk <vhdmatuk@gmail.com>
2025-06-09 13:13:59 -0300

    u

--- crud/Generator.php
+++ crud/Generator.php
@@ -671,7 +671,7 @@ class Generator extends \mootensai\enhancedgii\BaseGenerator
                 'filterWidgetOptions' => [
                     'pluginOptions' => ['allowClear' => true],
                 ],
-                'filterInputOptions' => ['placeholder' => '".$this->generateString(ucwords($humanize))."', 'id' => '$id']
+                'filterInputOptions' => ['placeholder' => ".$this->generateString(ucwords($humanize)).", 'id' => '$id']
             ],\n";
                 return $output;
             } else {
@@ -686,7 +686,7 @@ class Generator extends \mootensai\enhancedgii\BaseGenerator
                 'filterWidgetOptions' => [
                     'pluginOptions' => ['allowClear' => true],
                 ],
-                'filterInputOptions' => ['placeholder' => '".$this->generateString(ucwords($humanize))."', 'id' => '$id']
+                'filterInputOptions' => ['placeholder' => ".$this->generateString(ucwords($humanize)).", 'id' => '$id']
             ],\n";
                 return $output;
             }
