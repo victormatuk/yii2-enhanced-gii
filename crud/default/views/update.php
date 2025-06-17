66118660720a345e21fc048eb70e2b0055e73c91
Matuk <vhdmatuk@gmail.com>
2025-06-17 10:27:01 -0300

    u

--- crud/default/views/update.php
+++ crud/default/views/update.php
@@ -16,7 +16,12 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
 
-$this->title = <?= $generator->generateString('Update {modelClass}: ', ['modelClass' => Inflector::camel2words(StringHelper::basename($generator->modelClass))]) ?> . ' ' . $model-><?= $generator->getNameAttribute() ?>;
+<?php
+$modelClass = Inflector::camel2words(StringHelper::basename($generator->modelClass));
+?>
+$this->title = Yii::t('app', 'Update {modelClass}: ', [
+    'modelClass' => Yii::t('app', <?= var_export($modelClass, true) ?>),
+]) . ' ' . $model-><?= $generator->getNameAttribute() ?>;
 $this->params['breadcrumbs'][] = ['label' => <?= ($generator->pluralize) ? $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) : $generator->generateString(Inflector::camel2words(StringHelper::basename($generator->modelClass))) ?>, 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model-><?= $generator->getNameAttribute() ?>, 'url' => ['view', <?= $urlParams ?>]];
 $this->params['breadcrumbs'][] = <?= $generator->generateString('Update') ?>;
